9c33938d84002a25816362a23c6152a0a79fdcdc
Cesar Blum Silveira <cesars@microsoft.com>
2017-04-20 09:10:38 -0700

    wip

--- src/Microsoft.AspNetCore.Server.Kestrel.Core/Internal/Http/FrameRequestStream.cs
+++ src/Microsoft.AspNetCore.Server.Kestrel.Core/Internal/Http/FrameRequestStream.cs
@@ -12,7 +12,7 @@ namespace Microsoft.AspNetCore.Server.Kestrel.Core.Internal.Http
 {
     internal class FrameRequestStream : ReadOnlyStream
     {
-        private MessageBody _body;
+        private RequestBodyReader _requestBodyReader;
         private FrameStreamState _state;
         private Exception _error;
 
@@ -107,7 +107,7 @@ namespace Microsoft.AspNetCore.Server.Kestrel.Core.Internal.Http
             var task = ValidateState(cancellationToken);
             if (task == null)
             {
-                return _body.ReadAsync(new ArraySegment<byte>(buffer, offset, count), cancellationToken);
+                return _requestBodyReader.ReadAsync(new ArraySegment<byte>(buffer, offset, count), cancellationToken);
             }
             return task;
         }
@@ -126,18 +126,18 @@ namespace Microsoft.AspNetCore.Server.Kestrel.Core.Internal.Http
             var task = ValidateState(cancellationToken);
             if (task == null)
             {
-                return _body.CopyToAsync(destination, cancellationToken);
+                return _requestBodyReader.CopyToAsync(destination, cancellationToken);
             }
             return task;
         }
 
-        public void StartAcceptingReads(MessageBody body)
+        public void StartAcceptingReads(RequestBodyReader requestBodyReader)
         {
             // Only start if not aborted
             if (_state == FrameStreamState.Closed)
             {
                 _state = FrameStreamState.Open;
-                _body = body;
+                _requestBodyReader = requestBodyReader;
             }
         }
 
@@ -159,7 +159,7 @@ namespace Microsoft.AspNetCore.Server.Kestrel.Core.Internal.Http
             // Can't use dispose (or close) as can be disposed too early by user code
             // As exampled in EngineTests.ZeroContentLengthNotSetAutomaticallyForCertainStatusCodes
             _state = FrameStreamState.Closed;
-            _body = null;
+            _requestBodyReader = null;
         }
 
         public void Abort(Exception error = null)
